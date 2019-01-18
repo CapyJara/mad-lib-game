6a2b2d897467f515bffdf413e007d63722270505
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-01-18 10:50:56 -0800

    some more css

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function writeStory() {
     var noun_one = nounOne.value;
     
     
-    var story = noun_one + "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    var story = noun_one + 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
     
     var madLib = document.getElementById('madLib');
     madLib.textContent = story;
@@ -18,4 +18,4 @@ function writeStory() {
 
 }
 
-writeStory();
+//writeStory();
