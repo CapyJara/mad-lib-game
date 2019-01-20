180770a150368cf5d2863f781999bc67d1729ee3
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-01-19 16:40:34 -0800

    added story and inserted input fields

--- main.js
+++ main.js
@@ -4,18 +4,49 @@ function writeStory() {
     console.log(writeStory);
     
     // repeat for all inputs
-    var nounOne = document.getElementById('noun1');
+    var nameOneInput = document.getElementById('name1');
+    var nameTwoInput = document.getElementById('name2');
+    var nameThreeInput = document.getElementById('name3');
     
-    // repeat for all inputs
-    var noun_one = nounOne.value;
+    var nounInput = document.getElementById('noun');
+    
+    var meanAdjectiveInput = document.getElementById('meanAdjective');
+    
+    var emotionInput = document.getElementById('emotion');
+    
+    var movementInput = document.getElementById('movement');
+    
+    var obscenityInput = document.getElementById('obscenity');
+    
+    var verbOneInput = document.getElementById('verb1');
+    var verbTwoInput = document.getElementById('verb2');
+    var verbThreeInput = document.getElementById('verb3');
+    
+    var name_one = nameOneInput.value;
+    var name_two = nameTwoInput.value;
+    var name_three = nameThreeInput.value;
+    
+    var noun = nounInput.value;
+    
+    var mean_adjective = meanAdjectiveInput.value;
+    
+    var emotion = emotionInput.value;
+    
+    var movement = movementInput.value;
+    
+    var obscenity = obscenityInput.value;
+    
+    var verb_one = verbOneInput.value;
+    var verb_two = verbTwoInput.value;
+    var verb_three = verbThreeInput.value;
     
     
-    var story = noun_one + 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    // var story = 'bla bla bla' + noun_one + 
     
     var madLib = document.getElementById('madLib');
-    madLib.textContent = story;
-    
+
+    madLib.innerHTML = 'Ensign <strong>' + name_one + '</strong> walked in Ten Forward. They ordered a <strong>' + noun + '</strong> and began drinking it. Captain <strong>' + name_two + '</strong> walked in shortly after and with judgment and disgust for what they had ordered, called <strong>' + name_one + '</strong> a <strong>' + mean_adjective + '</strong>. Feeling <strong>' + emotion + '</strong> , <strong>' + name_one + ' ' + movement + '</strong> and shouted <strong>' + obscenity + '</strong> at Captain <strong>' + name_two + '</strong>. This did not go over well. Lieutenant <strong>' + name_three + '</strong> stepped in with great concern. Emotions grew tense. After several minutes of <strong>' + verb_one + '</strong>, they began to <strong>' + verb_two + '</strong>, and all ordered another round of <strong>' + noun + '</strong> and <strong>' + verb_three + '</strong> the night away.';
 
 }
 
-//writeStory();
+writeStory();
